539c028c7d74c1ec2c4d146e086b80172e43eec3
Dror Ivry <drorivry@gmail.com>
2017-10-05 20:51:41 +0300

    users to the sercer

--- UI/src/components/DrawerContent.js
+++ UI/src/components/DrawerContent.js
@@ -6,6 +6,8 @@ class DrawerContent extends Component {
     render() {
         return (
             <div className={'drawerContentWrapper'}>
+                <MenuItem value='general' icon={<img src={'https://d30y9cdsu7xlg0.cloudfront.net/png/29962-200.png'} height={23} width={23} />} caption='General Bucket' />
+                <MenuDivider/>
                 <MenuItem value='places' icon='place' caption='Places Bucket' />
                 <MenuItem value='munch' icon={<img src={'https://image.flaticon.com/icons/svg/27/27305.svg'} height={23} width={23} />} caption='Munch Bucket' />
                 <MenuItem value='hack' icon={<img src={'https://cdn0.iconfinder.com/data/icons/nature-food-and-kitchen/1000/file_light-43-512.png'} height={35} width={35} />} caption='Hack Bucket' />
