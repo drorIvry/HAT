5b0008bcbc908e0294d4d2ce61b3e1057097c953
Dror Ivry <drorivry@gmail.com>
2017-10-11 17:23:09 +0300

    added motions

--- server/src/routes/bucketActions.js
+++ server/src/routes/bucketActions.js
@@ -30,9 +30,12 @@ export function filterBucket(req, res) {
 export function addToBucket(req, res) {
   var newActivity = new Activity(req.body);
 
+  if((req.body.name === undefined)||(req.body.address === undefined))
+    return res.send(400,"bad request");
+
   newActivity.save((err) => {
     if (err)
-      return res.send(500, {error: err});
+      return res.send(2000, {error: err});
 
     return res.send("successfully saved");
   })
